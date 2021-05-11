8ae87af0362d8feadd215766af58c619be4e2df8
Jeshu Brij <jeshu911@gmail.com>
2021-05-11 08:29:09 +0530

    API server added with workspace

--- api/server/index.js
+++ api/server/index.js
@@ -1,20 +1,56 @@
-const express = require('express')
-const next = require('next')
+// import cors from 'cors';
+import express from 'express';
+import nextApp from '@droan-app/webapp';
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const port = process.env.PORT || 3001;
+const handle = nextApp.getRequestHandler();
 
-app.prepare().then(() => {
-  const server = express()
+async function createServer() {
+  try {
+    // 1. create mongoose connection
+    // await createSession();
+    // 2. create express server
+    const app = express();
 
-  server.all('*', (req, res) => {
-    return handle(req, res)
-  })
+    // allow CORS from client app
+    // const corsOptions = {
+    //   origin: 'http://localhost:3000',
+    //   credentials: true,
+    // };
+    // app.use(cors(corsOptions));
 
-  server.listen(port, (err) => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
-  })
-})
+    // allow JSON requests
+    app.use(express.json());
+
+    // const schema = await createSchema();
+
+    // 3. create GraphQL server
+    // const apolloServer = new ApolloServer({
+    //   schema,
+    //   context: ({ req, res }) => ({ req, res }),
+    //   introspection: true,
+    //   // enable GraphQL Playground with credentials
+    //   playground: {
+    //     settings: {
+    //       'request.credentials': 'include',
+    //     },
+    //   },
+    // });
+
+    // apolloServer.applyMiddleware({ app, cors: corsOptions });
+
+    await nextApp.prepare();
+    app.get('*', (req, res) => handle(req, res));
+
+    // start the server
+    app.listen({ port }, () => {
+      console.log(
+        `🚀 Server ready at http://localhost:${port}`
+      );
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+createServer();
